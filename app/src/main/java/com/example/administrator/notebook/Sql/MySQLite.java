185c6e6aa9a2b1ac3d5941052dc45016f4096165
MiChong <1564666023@qq.com>
2017-03-02 10:19:07 +0800

    数据库设计

--- app/src/main/java/com/example/administrator/notebook/Sql/MySQLite.java
+++ app/src/main/java/com/example/administrator/notebook/Sql/MySQLite.java
@@ -1,8 +1,27 @@
 package com.example.administrator.notebook.Sql;
 
+import java.sql.SQLData;
+import java.sql.SQLException;
+import java.sql.SQLInput;
+import java.sql.SQLOutput;
+
 /**
  * Created by Administrator on 2017/2/24 0024.
  */
 
-public class MySQLite {
+public class MySQLite implements SQLData {
+    @Override
+    public String getSQLTypeName() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void readSQL(SQLInput stream, String typeName) throws SQLException {
+
+    }
+
+    @Override
+    public void writeSQL(SQLOutput stream) throws SQLException {
+
+    }
 }
