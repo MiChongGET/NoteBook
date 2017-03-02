d58a7141820872a1b1b936553f22fbe01910b525
MiChong <1564666023@qq.com>
2017-03-02 10:41:52 +0800

    数据库设计

---
+++ app/src/main/java/com/example/administrator/notebook/Sql/UserInterface.java
@@ -0,0 +1,21 @@
+package com.example.administrator.notebook.Sql;
+
+/**
+ * Created by Administrator on 2017/3/2 0002.
+ */
+
+public abstract class UserInterface {
+
+    //读取数据库获取日记
+    public abstract void readSql();
+
+    //删除日记
+    public abstract void deletSql();
+
+    //添加日记
+    public abstract void addSql();
+
+    //修改日记
+    public abstract void updateSql();
+
+}
